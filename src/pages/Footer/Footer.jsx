828bbaced1d3d533bdbe425d1eba3f3c17f39802
jaycode8 <jamesmumo443@gmail.com>
2024-07-11 11:34:46 +0300

    updates

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -3,12 +3,19 @@ import './Footer.scss';
 
 const Footer = () => {
     return (
-        <div className='footer grid'>
-            <div className='copyright grid'>
-                <span>&copy;<small>  {new Date().getFullYear()}</small> <a href='#'> FilmHub </a> All Rights Reserved.</span>
+        <div className="footer grid">
+            <div className="copyright grid">
+                <span>
+                    &copy;<small> {new Date().getFullYear()}</small> <a href="#"> FilmHub </a> All Rights Reserved.
+                </span>
             </div>
-            <div className='developer grid'>
-                <span>made with love by <a href='https://jamesmumo-cf8e5.web.app' target='_blank'> Jaymoh </a></span>
+            <div className="developer grid">
+                <span>
+                    made with <span>❤️</span> by
+                    <a href="https://jamesmumo.vercel.app/" target="_blank">
+                        <strong> Jaymoh </strong>
+                    </a>
+                </span>
             </div>
         </div>
     );
